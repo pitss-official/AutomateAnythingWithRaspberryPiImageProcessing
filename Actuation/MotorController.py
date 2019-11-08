0bdb74df71da8e6793796038d6be72601d6dd828
Pawan Kumar <34182155+pitss-official@users.noreply.github.com>
2019-11-08 18:08:55 +0530

    Create MotorController.py

---
+++ Actuation/MotorController.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(03, GPIO.OUT)
+GPIO.setup(05, GPIO.OUT)
+GPIO.setup(07, GPIO.OUT)
+
+pwm=GPIO.PWM(07, 100)
+pwm.start(0)
+GPIO.output(03, True)
+GPIO.output(05, False)
+pwm.ChangeDutyCycle(50)
+GPIO.output(07, True)
+sleep(2)
+GPIO.output(07, False)
+GPIO.output(03, False)
+GPIO.output(05, True)
+pwm.ChangeDutyCycle(75)
+GPIO.output(07, True)
+sleep(3)
+GPIO.output(07, False)
+pwm.stop()
+GPIO.cleanup()
